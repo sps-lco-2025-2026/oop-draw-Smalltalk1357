6f7b942fbeac74e84d7a232df8f801b6cf1f3ae0
Smalltalk1357 <alex.wan@icloud.com>
2026-01-28 12:07:10 +0000

    Updated Name

--- OOPDraw/OOPDraw.cs
+++ OOPDraw/OOPDraw.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace OOPDraw
 {
-    public partial class Form1 : Form
+    public partial class OOPDraw : Form
     {
-        public Form1()
+        public OOPDraw()
         {
             InitializeComponent();
         }
