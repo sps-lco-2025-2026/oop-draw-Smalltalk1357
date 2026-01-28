c21d96329f86748aa145f6bb97d1448ca09794ff
Smalltalk1357 <alex.wan@icloud.com>
2026-01-28 12:51:27 +0000

    Update

--- OOPDraw/OOPDraw.Designer.cs
+++ OOPDraw/OOPDraw.Designer.cs
@@ -28,13 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OOPDraw));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-136, -47);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1339, 641);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // OOPDraw
+            // 
+            this.AccessibleName = "McAfee Anti-Virus";
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.Alert;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1006, 673);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Cursor = System.Windows.Forms.Cursors.Help;
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "OOPDraw";
+            this.Text = "McAfee Anti-Virus";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- OOPDraw/OOPDraw.cs
+++ OOPDraw/OOPDraw.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,21 @@ namespace OOPDraw
         public OOPDraw()
         {
             InitializeComponent();
+            DoubleBuffered = true;
+        }
+
+        private Bitmap MyImage;
+        private void pictureBox1_Click(String fileToDisplay, int xSize, int ySize)
+        {
+            if (MyImage != null)
+            {
+                MyImage.Dispose();
+            }
+
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            MyImage = new Bitmap(fileToDisplay);
+            pictureBox1.ClientSize = new Size(xSize, ySize);
+            pictureBox1.Image = (Image)MyImage;
         }
     }
 }
