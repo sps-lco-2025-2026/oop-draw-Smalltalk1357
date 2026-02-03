5ef9b6690a12647ea5c9c25f675bcbc2ae52f9c8
Smalltalk1357 <alex.wan@icloud.com>
2026-02-03 11:57:13 +0000

    Ex 1

--- OOPDraw/OOPDraw.Designer.cs
+++ OOPDraw/OOPDraw.Designer.cs
@@ -29,28 +29,45 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OOPDraw));
+            this.Canvas = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Canvas)).BeginInit();
             this.SuspendLayout();
+            // 
+            // Canvas
+            // 
+            this.Canvas.BackColor = System.Drawing.SystemColors.Window;
+            this.Canvas.Location = new System.Drawing.Point(12, 12);
+            this.Canvas.Name = "Canvas";
+            this.Canvas.Size = new System.Drawing.Size(982, 649);
+            this.Canvas.TabIndex = 0;
+            this.Canvas.TabStop = false;
+            this.Canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.Canvas_Paint);
             // 
             // OOPDraw
             // 
             this.AccessibleName = "McAfee Anti-Virus";
-            this.AccessibleRole = System.Windows.Forms.AccessibleRole.Alert;
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.Graphic;
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
             this.ClientSize = new System.Drawing.Size(1006, 673);
-            this.Cursor = System.Windows.Forms.Cursors.Help;
+            this.Controls.Add(this.Canvas);
+            this.Cursor = System.Windows.Forms.Cursors.Cross;
             this.DoubleBuffered = true;
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(5);
             this.Name = "OOPDraw";
-            this.Text = "McAfee Anti-Virus";
+            this.Text = "Drawing Program";
+            ((System.ComponentModel.ISupportInitialize)(this.Canvas)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox Canvas;
     }
 }
 
--- OOPDraw/OOPDraw.cs
+++ OOPDraw/OOPDraw.cs
@@ -1,12 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace OOPDraw
@@ -17,6 +10,18 @@ namespace OOPDraw
         {
             InitializeComponent();
             DoubleBuffered = true;
+        }
+
+        Pen currentPen = new Pen(Color.Black);
+
+        private void Canvas_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics gr = e.Graphics;
+            Point a = new Point(20, 30);
+            Point b = new Point(400, 500);
+            gr.DrawLine(currentPen, a, b);
+
+            gr.DrawLine()
         }
     }
 }
