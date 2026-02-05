3269122bb46d2ec987b85fe3215aed337831b0d7
Smalltalk1357 <alex.wan@icloud.com>
2026-02-05 16:19:06 +0000

    Ex 6

--- OOPDraw/OOPDraw.Designer.cs
+++ OOPDraw/OOPDraw.Designer.cs
@@ -30,18 +30,89 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OOPDraw));
             this.Canvas = new System.Windows.Forms.PictureBox();
+            this.LineWidth = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Colour = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.Canvas)).BeginInit();
             this.SuspendLayout();
             // 
             // Canvas
             // 
             this.Canvas.BackColor = System.Drawing.SystemColors.Window;
-            this.Canvas.Location = new System.Drawing.Point(12, 12);
+            this.Canvas.Location = new System.Drawing.Point(250, -1);
             this.Canvas.Name = "Canvas";
-            this.Canvas.Size = new System.Drawing.Size(982, 649);
+            this.Canvas.Size = new System.Drawing.Size(983, 776);
             this.Canvas.TabIndex = 0;
             this.Canvas.TabStop = false;
             this.Canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.Canvas_Paint);
+            this.Canvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Canvas_MouseDown);
+            this.Canvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Canvas_MouseMove);
+            this.Canvas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Canvas_MouseUp);
+            // 
+            // LineWidth
+            // 
+            this.LineWidth.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LineWidth.FormattingEnabled = true;
+            this.LineWidth.Items.AddRange(new object[] {
+            "Thin",
+            "Medium",
+            "Thick"});
+            this.LineWidth.Location = new System.Drawing.Point(17, 74);
+            this.LineWidth.Name = "LineWidth";
+            this.LineWidth.Size = new System.Drawing.Size(210, 33);
+            this.LineWidth.TabIndex = 1;
+            this.LineWidth.SelectedIndexChanged += new System.EventHandler(this.LineWidth_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Design";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Line Width";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Colour";
+            // 
+            // Colour
+            // 
+            this.Colour.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Colour.FormattingEnabled = true;
+            this.Colour.Items.AddRange(new object[] {
+            "Red",
+            "Orange",
+            "Yellow",
+            "Green",
+            "Blue",
+            "Purple",
+            "Black",
+            "Grey"});
+            this.Colour.Location = new System.Drawing.Point(17, 148);
+            this.Colour.Name = "Colour";
+            this.Colour.Size = new System.Drawing.Size(210, 33);
+            this.Colour.TabIndex = 5;
+            this.Colour.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // OOPDraw
             // 
@@ -50,7 +121,13 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
-            this.ClientSize = new System.Drawing.Size(1006, 673);
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(1233, 775);
+            this.Controls.Add(this.Colour);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LineWidth);
             this.Controls.Add(this.Canvas);
             this.Cursor = System.Windows.Forms.Cursors.Cross;
             this.DoubleBuffered = true;
@@ -62,12 +139,18 @@
             this.Text = "Drawing Program";
             ((System.ComponentModel.ISupportInitialize)(this.Canvas)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox Canvas;
+        private System.Windows.Forms.ComboBox LineWidth;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox Colour;
     }
 }
 
--- OOPDraw/OOPDraw.cs
+++ OOPDraw/OOPDraw.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace OOPDraw
@@ -13,15 +15,92 @@ namespace OOPDraw
         }
 
         Pen currentPen = new Pen(Color.Black);
+        bool dragging = false;
+        Point startOfDrag = Point.Empty;
+        Point lastMousePosition = Point.Empty;
+        List<Line> lines = new List<Line>();
 
         private void Canvas_Paint(object sender, PaintEventArgs e)
         {
             Graphics gr = e.Graphics;
-            Point a = new Point(20, 30);
-            Point b = new Point(400, 500);
-            gr.DrawLine(currentPen, a, b);
+            foreach (Line line in lines)
+            {
+                line.Draw(gr);
+            }
+        }
 
-            gr.DrawLine()
+        private void Canvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            dragging = true;
+            startOfDrag = lastMousePosition = e.Location;
+            lines.Add(new Line(currentPen, e.X, e.Y));
+        }
+
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (dragging)
+            {
+                Line currentLine = lines.Last();
+                currentLine.GrowTo(e.X, e.Y);
+                lastMousePosition = e.Location;
+                Refresh();
+            }
+        }
+
+        private void Canvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            dragging = false;
+        }
+
+        private void LineWidth_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            float width = currentPen.Width;
+            switch (LineWidth.Text)
+            {
+                case "Thin":
+                    width = 2.0F;
+                    break;
+                case "Medium":
+                    width = 4.0F;
+                    break;
+                case "Thick":
+                    width = 8.0F;
+                    break;
+            }
+            currentPen = new Pen(currentPen.Color, width);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Color color = currentPen.Color;
+            switch (Colour.Text)
+            {
+                case "Red":
+                    color = Color.Red;
+                    break;
+                case "Orange":
+                    color = Color.Orange;
+                    break;
+                case "Yellow":
+                    color = Color.Yellow;
+                    break;
+                case "Green":
+                    color = Color.Green;
+                    break;
+                case "Blue":
+                    color = Color.Blue;
+                    break;
+                case "Purple":
+                    color = Color.Purple;
+                    break;
+                case "Black":
+                    color = Color.Black;
+                    break;
+                case "Grey":
+                    color = Color.Gray;
+                    break;
+            }
+            currentPen = new Pen(color, currentPen.Width);
         }
     }
 }
